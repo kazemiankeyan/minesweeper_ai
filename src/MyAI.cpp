10b11e15fea6e2c7f3ab5add208580cb3983bed4
LostABike <hndinh1@uci.edu>
2020-06-08 22:31:57 -0700

    fk float

--- src/MyAI.cpp
+++ src/MyAI.cpp
@@ -599,8 +599,8 @@ Agent::Action MyAI::getAction( int number )
           int e = std::stoi(board[r][c]) - getType(c, r, "-1"); //effective
           int unc = getType(c, r, ".");
           if (unc > 0){
-            float z = (e/unc)*100;
-            int percent_bomb = (int)z;
+            float z = (float)e/unc;
+            int percent_bomb = (int)100*z;
             if (overlap.size() > 0){
               vector<int> temp = *overlap.begin();
               frontier.push({temp[0], temp[1], percent_bomb});
